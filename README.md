6b58858a300e3920a13c206766ebbdcba256e92a
imdhruv99 <dpdhruvprajapati@gmail.com>
2020-06-20 12:08:48 +0530

    Rock Paper Scissor Game

---
+++ README.md
@@ -0,0 +1,23 @@
+# Rock Paper Scissor Game
+- Rock Paper Scissor Game play implemented in Python3 and tkinter
+
+# Game play
+- In Traditional approch, The players may count aloud to three, or speak the name of the game, 
+either raising one hand in a fist and swinging it down with each syllable or holding it behind their back. 
+- They then "throw" by extending it towards their opponent. 
+Variations include a version where players throw immediately on the third count,
+or a version where they shake their hands three times before "throwing".
+
+# Module used
+- tkinter
+
+# Installation
+- ```pip3 install -U tkinter```
+
+# Running The Project
+- ```Python rps.py```
+
+# Author
+- D H R U V &nbsp; P R A J A P A T I
+
+# Thank You!
---
+++ rps.py
@@ -0,0 +1,142 @@
+import tkinter
+
+from tkinter import messagebox
+
+import random
+
+
+top = tkinter.Tk()
+
+top.title("Rock | Paper | Scissor")
+
+
+userScore = 0
+compScore = 0
+
+
+def Rock():
+
+    """ Rock Function with 3 comparision"""
+
+    global userScore, compScore
+
+    comp = random.randint(1,3)
+
+    if comp == 3:
+
+        comp = "Scissor"
+
+        userScore += 1
+
+        messagebox.showinfo("Congratulation!", "You WIN!! \n""Your Choice:Rock"+"\nComp Choice: " \
+                            +comp+"\nYour Score: "+str(userScore)+"\nComputer Score: "+str(compScore))
+
+    elif comp == 1:
+
+        comp = "Rock"
+
+        messagebox.showinfo("Same pinch!!",
+                            "IT'S A TIE!! !!\n" + "Your Choice:Rock" + "\nComp Choice: " + comp + "\nYour Score: "
+                            + str(userScore) + "\nComputer Score: " + str(compScore))
+
+
+    else:
+
+        comp = "Paper"
+
+        compScore += 1
+
+        messagebox.showinfo("Hard Luck!!",
+                            "YOU LOOSE!!\n" + "Your Choice:Rock" + "\nComp Choice: " + comp + "\nYour Score: "
+                            + str(userScore) + "\nComputer Score: " + str(compScore))
+
+
+def Scissor():
+
+    """ Scissor Function with 3 comparision"""
+
+    global userScore, compScore
+
+    comp = random.randint(1, 3)
+
+    if comp == 2:
+
+        comp = "Paper"
+
+        userScore += 1
+
+        messagebox.showinfo("Congratulation!!",
+                            "YOU WIN!!\nYour Choice: Scissor\n" + "Comp choice: " + comp + "\nYour Score: " + str(
+                                userScore) + "\nComputer Score: " + str(compScore))
+
+    elif comp == 3:
+        
+        comp = "Scissor"
+        
+        messagebox.showinfo("Same pinch!!",
+                            "IT'S A TIE!!\nYour Choice: Scissor\n" + "Comp choice: " + comp + "\nYour Score: " + str(
+                                userScore) + "\nComputer Score: " + str(compScore))
+
+
+    else:
+        
+        comp = "Rock"
+        
+        compScore += 1
+        
+        messagebox.showinfo("Hard Luck!!",
+                            "YOU LOOSE!!\nYour Choice: Scissor\n" + "Comp choice:" + comp + "\nYour Score: " + str(
+                                userScore) + "\nComputer Score: " + str(compScore))
+
+def Paper():
+    
+    """ Paper Function with 3 comparision"""
+
+    global userScore, compScore
+
+    comp = random.randint(1, 3)
+
+    if comp == 1:
+
+        comp = "Rock"
+
+        userScore += 1
+
+        messagebox.showinfo("Congratulation!!",
+                            "YOU WIN!!\nYour Choice: Scissor\n" + "Comp choice: " + comp + "\nYour Score: " + str(
+                                userScore) + "\nComputer Score: " + str(compScore))
+
+    elif comp == 2:
+        
+        comp = "Paper"
+        
+        messagebox.showinfo("Same pinch!!",
+                            "IT'S A TIE!!\nYour Choice: Scissor\n" + "Comp choice: " + comp + "\nYour Score: " + str(
+                                userScore) + "\nComputer Score: " + str(compScore))
+
+
+    else:
+        
+        comp = "Scissor"
+        
+        compScore += 1
+        
+        messagebox.showinfo("Hard Luck!!",
+                            "YOU LOOSE!!\nYour Choice: Scissor\n" + "Comp choice:" + comp + "\nYour Score: " + str(
+                                userScore) + "\nComputer Score: " + str(compScore))
+
+# Declaring Buttons
+
+B1 = tkinter.Button(top, text = "Rock",bg='red',height="10",width="20", command = Rock)
+
+B2 = tkinter.Button(top, text = "Paper",bg='green',height="10",width="20", command = Paper)
+
+B3 = tkinter.Button(top, text = "Scissor",bg='blue',height="10",width="20", command = Scissor)
+
+B1.pack(side='left')
+
+B2.pack(side='left')
+
+B3.pack(side='left')
+
+top.mainloop()
